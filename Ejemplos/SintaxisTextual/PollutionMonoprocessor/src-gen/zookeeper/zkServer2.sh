ede0fb4d19d42ae2594ce91e9a5a16e2de5097e7
Julio Escudero Cuesta <escuderocuestajulio@gmail.com>
2025-10-05 12:36:56 +0200

    Files deleted

--- Ejemplos/SintaxisTextual/PollutionMonoprocessor/src-gen/zookeeper/zkServer2.sh
+++
@@ -1,3 +0,0 @@
-cd /home/gestor/apache/apps/pollution/topics/scripts
-/home/gestor/apache/services/kafka/bin/kafka-topics.sh start /home/gestor/apache/apps/pollution/topics/config/zoo2.cfg
-sleep 30
--- SintaxisVisual/Prueba/caseStudies/Pollution/PollutionHybridKubernetes/gen/LocalNode/zkServer0.sh
+++
@@ -1,1 +0,0 @@
-helm install zoo0 \src\main\nodes\zookeeper\helm -f C:\Temp\helm\zoo0values.yaml --kubeconfig C:\\temp\\kubernetes\\kubeConfig
